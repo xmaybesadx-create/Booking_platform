351fce239798a0b185d5d61feb32720329c3b337
Maria <xmaybesadx@gmail.com>
2026-01-21 12:36:52 +0200

    Init frontend with React + Tailwind

---
+++ frontend/src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white text-3xl">
+      Tailwind работает 🚀
+    </div>
+  )
+}
